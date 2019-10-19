e0ffa347b3a868bbb28c1e4cdbd0ca01190d29ba
Ryu <dbwodlf3@naver.com>
2019-10-20 01:33:19 +0900

    bug fix and added Volume option..

--- mmlparser.js
+++ mmlparser.js
@@ -3,6 +3,7 @@ var Data5 = "ass8vv88"
 var test = /[0-8]/
 
 var Data = `f+
+v4
 f
 d+
 c+4
@@ -19,9 +20,10 @@ g
 g
 g
 g`
+
 var Data2 = "abcdefgabcdefg"
 var Data3 = ".f+"
-var Data4 = `MML@v15l2f+fd+c+4g+4l32c+f+l4.b&b16a+a8a+l16&a+d+l8gd+a+>d+l2c+c-c<f+4f4d+d+l4>d+dd+fl32<f+b>f4.&f16d+4c+8<b8a+8r4.g+2c-d+a+4.&a+16a+4b4>c+2<f+2c-d+l8a+.n44a+n46a+c-a+n46a+da+n46ra+l16>da+r8b8dfc-dn58d<g+bfg+df<b8g+8f>dfba+8g+8f+fdn44r8f8>a+>fn70d<fbda+n58fn56d<f+bd+a+dg+c-fc-bd+b>ed+l32<a+b>c+dd+8l16n75ba+fd+<bg+fd+<bg+fd+c-d+8o6d+<bg+f+fd+c<af+fd+fa>cr8a+>fn68d<fbda+n58fn56d<fba+fa+bn61ba+aa+8r8l4f+f+fd+c+l32c+f+l8.ba+l16aa+4d+ga+>d+l8c+<ba+bb+d+f+fd+>d+f+fd+dd+ff+.l16n58fd+c+<ba+8c+f+a+32b32a+g+bl8a+g+a+b>c+4<f+fd+.l16a+ba+g+a+n46a+da+d+a+fa+d+8f+a+>fd+c+<ba+n63g+n63f+n63fn63l8d+d+d+dd+l16a+ba+g+fdl8rn46d+4.n46f4.d+d+dd+l16a+ba+ba+g+g8>d+ed+c+<ba+l8n61ba+bfl16>c+d+c+<bg+al8a+fd+c+fl16a+ba+aa+dfd+dd+f+d+a+b>d+c+cd+c+8c+8<f+8g+n61f+n61fn61a+>c+f+fd+c+cd+c+<g+f+ff+c+f+>c+<c-g+d+g+b+g+d+g+f+fd+c+g+c+n61f>f+g+f+fd+c+cd+c+<g+f+ff+c+f+>c+<c-g+d+g+a+g+f+ff+a+8a+da+da+da+aa+fa+d+a+da+aa+fa+d+a+da+fa+>f<a+a+8a+32>d+32a+l4&a+.<f+fd+c+l32c+f+l8.ba+l16aa+4d+ga+>d+l8c+<ba+bb+d+f+fd+>d+f+fd+dd+ff+.l16n58fd+c+<bl8a+rg+4a+g+a+b>c+4<f+fd+.l16a+ba+g+a+n46a+da+d+a+fa+d+8f+a+>fd+c+<ba+n63g+n63f+n63fn63l8d+d+d+dd+l16a+ba+g+fd<d+8>f+a+>fd+c+<ba+n63g+n63f+n63f>d+l8d+d+d+dd+l16<a+ba+ba+g+g8>d+ev13<<a+8>c+8,t160v13l2<a+rg+r1r1.>g+r1r1n63l4d+dd+fr2bre8r.e2r2c+ra+2r1.<d.l16>ddv14ddddv15d8d8v12o1a+1v15a+2.v13o4dr4r<a+8r1.n61r4rv15b4.r32v11>a+.g+32a+.g+32a+.g+32a+.r8v15c2.r4f>f<d>d<c-bn46a+<f>f<d>d<c-ba+2v11>>g+32a+.r8v13l4<d<a+f+g+g+a+a+a+a+bb>cc<a+a+a+a+>d+d+c+c+<bba+a+g+g+<<a+l16a+a+a+a+>>d+2r4>>ff+g+a+d+fff+a+g+fdd+ba+ba+g+fdv15<<d+8>f+a+>fd+c+<ba+n63g+n63f+n63f>d+l8d+d+d+dd+l16<<a+ba+a+a+a+r1.r8>>f8d+8c+8r1<d+c+cd+c+8c+8r2v13l4<f+fd+<a+b>cc+<c+8c+8>f+fd+v15>c+v13<c-c+8c+8f+d8d8<a+1&a+1>a+rg+r2.>d+r2.ar.l8d+f+fd+dd+fa+4r4ere4r1.o1a+4l16a+a+a+a+>>d+2r4>>ff+g+a+d+fff+a+g+fdd+ba+ba+g+fdl8rv15<<a+>d+4.n46f4.d+d+dd+l16<a+ba+a+a+a+r4>>d+c+<ba+l8n61ba+bfl16>c+d+c+<bg+al8a+fd+c+fl16a+ba+aa+dfd+dd+f+d+a+g+n61ba+ba+r>drd+<a+f+>d+<<a+>fg+>dd+<a+f+>d+<<a+>fg+>dd+<a+f+>d+<<a+>fg+>dd+<a+f+>d+<<a+>f>dn58d+1,v13o2d+a+>f+2<f>c+f2n30c+n49a+<g>d+2.n32d+b2<a>d+2.n34fa+2<<a+>g+>d2c-f+b2c+8r.<f+2c-f+>c+2n34f+2.<<g+>d+l2ba+<<a+v15l1a+&a+>d+&d+l8g+>>d+2.rn21f4.f2o0a+1r2v13n54ra+4l2>d+ff+gg+aa+n10d+c+<ba+g+a+>d+c+l4<ba+n15a+>d+2c+2<ba+n15a+l8>>g>d+a+>c+<<g+>d+g+b<c+g+>c+f<<f+>c+g+4da+>f4<d+a+<c+4c4c+c+l4f+>c+f+fd+<a+b>cc+<c+8c+8>f+fd+<a+bl8>c+c+f+4dd<<a+1&a+1l2>>d+ff+gg+aa+n22d+c+8r8f+4ba+<g+4g+4n10d+c+l4<ba+n15a+>d+2c+2<ba+n15a+l8>>gn39r4g+>d+g+b<c+g+>c+f<<f+>c+g+4da+>f4<d+a+l4c+<ba+n27a+n27a+n27v44a+n27a+>d+1;`
+var Data4 = `MML@v15l2f+fd+c+4g+4l32c+f+l4.b&b16a+a8a+l16&a+d+l8gd+a+>d+l2c+c-c<f+4f4d+d+l4>d+dd+fl32<f+b>f4.&f16d+4c+8<b8a+8r4.g+2c-d+a+4.&a+16a+4b4>c+2<f+2c-d+l8a+.n44a+n46a+c-a+n46a+da+n46ra+l16>da+r8b8dfc-dn58d<g+bfg+df<b8g+8f>dfba+8g+8f+fdn44r8f8>a+>fn70d<fbda+n58fn56d<f+bd+a+dg+c-fc-bd+b>ed+l32<a+b>c+dd+8l16n75ba+fd+<bg+fd+<bg+fd+c-d+8o6d+<bg+f+fd+c<af+fd+fa>cr8a+>fn68d<fbda+n58fn56d<fba+fa+bn61ba+aa+8r8l4f+f+fd+c+l32c+f+l8.ba+l16aa+4d+ga+>d+l8c+<ba+bb+d+f+fd+>d+f+fd+dd+ff+.l16n58fd+c+<ba+8c+f+a+32b32a+g+bl8a+g+a+b>c+4<f+fd+.l16a+ba+g+a+n46a+da+d+a+fa+d+8f+a+>fd+c+<ba+n63g+n63f+n63fn63l8d+d+d+dd+l16a+ba+g+fdl8rn46d+4.n46f4.d+d+dd+l16a+ba+ba+g+g8>d+ed+c+<ba+l8n61ba+bfl16>c+d+c+<bg+al8a+fd+c+fl16a+ba+aa+dfd+dd+f+d+a+b>d+c+cd+c+8c+8<f+8g+n61f+n61fn61a+>c+f+fd+c+cd+c+<g+f+ff+c+f+>c+<c-g+d+g+b+g+d+g+f+fd+c+g+c+n61f>f+g+f+fd+c+cd+c+<g+f+ff+c+f+>c+<c-g+d+g+a+g+f+ff+a+8a+da+da+da+aa+fa+d+a+da+aa+fa+d+a+da+fa+>f<a+a+8a+32>d+32a+l4&a+.<f+fd+c+l32c+f+l8.ba+l16aa+4d+ga+>d+l8c+<ba+bb+d+f+fd+>d+f+fd+dd+ff+.l16n58fd+c+<bl8a+rg+4a+g+a+b>c+4<f+fd+.l16a+ba+g+a+n46a+da+d+a+fa+d+8f+a+>fd+c+<ba+n63g+n63f+n63fn63l8d+d+d+dd+l16a+ba+g+fd<d+8>f+a+>fd+c+<ba+n63g+n63f+n63f>d+l8d+d+d+dd+l16<a+ba+ba+g+g8>d+ev13<<a+8>c+8,t160v13l2<a+rg+r1r1.>g+r1r1n63l4d+dd+fr2bre8r.e2r2c+ra+2r1.<d.l16>ddv14ddddv15d8d8v12o1a+1v15a+2.v13o4dr4r<a+8r1.n61r4rv15b4.r32v11>a+.g+32a+.g+32a+.g+32a+.r8v15c2.r4f>f<d>d<c-bn46a+<f>f<d>d<c-ba+2v11>>g+32a+.r8v13l4<d<a+f+g+g+a+a+a+a+bb>cc<a+a+a+a+>d+d+c+c+<bba+a+g+g+<<a+l16a+a+a+a+>>d+2r4>>ff+g+a+d+fff+a+g+fdd+ba+ba+g+fdv15<<d+8>f+a+>fd+c+<ba+n63g+n63f+n63f>d+l8d+d+d+dd+l16<<a+ba+a+a+a+r1.r8>>f8d+8c+8r1<d+c+cd+c+8c+8r2v13l4<f+fd+<a+b>cc+<c+8c+8>f+fd+v15>c+v13<c-c+8c+8f+d8d8<a+1&a+1>a+rg+r2.>d+r2.ar.l8d+f+fd+dd+fa+4r4ere4r1.o1a+4l16a+a+a+a+>>d+2r4>>ff+g+a+d+fff+a+g+fdd+ba+ba+g+fdl8rv15<<a+>d+4.n46f4.d+d+dd+l16<a+ba+a+a+a+r4>>d+c+<ba+l8n61ba+bfl16>c+d+c+<bg+al8a+fd+c+fl16a+ba+aa+dfd+dd+f+d+a+g+n61ba+ba+r>drd+<a+f+>d+<<a+>fg+>dd+<a+f+>d+<<a+>fg+>dd+<a+f+>d+<<a+>fg+>dd+<a+f+>d+<<a+>f>dn58d+1,v13o2d+a+>f+2<f>c+f2n30c+n49a+<g>d+2.n32d+b2<a>d+2.n34fa+2<<a+>g+>d2c-f+b2c+8r.<f+2c-f+>c+2n34f+2.<<g+>d+l2ba+<<a+v15l1a+&a+>d+&d+l8g+>>d+2.rn21f4.f2o0a+1r2v13n54ra+4l2>d+ff+gg+aa+n10d+c+<ba+g+a+>d+c+l4<ba+n15a+>d+2c+2<ba+n15a+l8>>g>d+a+>c+<<g+>d+g+b<c+g+>c+f<<f+>c+g+4da+>f4<d+a+<c+4c4c+c+l4f+>c+f+fd+<a+b>cc+<c+8c+8>f+fd+<a+bl8>c+c+f+4dd<<a+1&a+1l2>>d+ff+gg+aa+n22d+c+8r8f+4ba+<g+4g+4n10d+c+l4<ba+n15a+>d+2c+2<ba+n15a+l8>>gn39r4g+>d+g+b<c+g+>c+f<<f+>c+g+4da+>f4<d+a+v3l4c+<ba+n27a+n27a+n27v4a+n27a+>d+1;`
 
 sepreateMMLtoNote(Data4)
 //Objects
@@ -41,8 +43,8 @@ const Note = {
 function sepreateMMLtoNote(Data){
     Data = sepreateDataToUnit(Data)
     var regPitch = /[abcdefgn][+-]?/
-    var regOption = /[lo><]/
-    var regRange = [/[0-8]/, /1[0-5] |[0-9]/, /6[0-4]|[1-5][0-9]|[1-9]/,  /8[0-3] |[1-7][0-9] |[1-9]/ ,/25[0-5] |2[0-4][0-9] |1[0-9][0-9] |[4-9][0-9] |3[2-9]/]
+    var regOption = /[lov><]/
+    var regRange = [/[0-8]/, /1[0-5]|[0-9]/, /6[0-4]|[1-5][0-9]|[1-9]/,  /8[0-3] |[1-7][0-9] |[1-9]/ ,/25[0-5] |2[0-4][0-9] |1[0-9][0-9] |[4-9][0-9] |3[2-9]/]
     //0)8~0까지, 1)15~0까지, 2)64~1까지, 3)83~1까지, 4)255~32까지,
 
     //noteSetValue
@@ -71,7 +73,7 @@ function sepreateMMLtoNote(Data){
             if(x.match("<")){if(defaultOctave>0){defaultOctave--}}
             if(x.match("o")){if(x.match(regRange[0])){defaultOctave = x.match(regRange[0])}}
             if(x.match("l")){if(x.match(regRange[4])){defaultTempo = x.match(regRange[4])}}
-            if(x.match("v")){if(x.match(regRange[1])){defaultTempo = x.match(regRange[1])}}
+            if(x.match("v")){if(x.match(regRange[1])){defaultVolume = x.match(regRange[1])[0];}}
         }
     })
 
